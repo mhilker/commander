01ac1e94a616aca9b94c360e5a7a90ff502260f9
Maik Hilker <m.hilker@synatix.com>
2019-03-02 17:53:26 +0100

    fixed typehint

--- src/Event/DirectEventBus.php
+++ src/Event/DirectEventBus.php
@@ -6,7 +6,7 @@ namespace Commander\Event;
 
 final class DirectEventBus implements EventBus
 {
-    /** @var EventHandler[] */
+    /** @var EventHandler[]|EventHandlers */
     private $handlers;
 
     public function __construct(EventHandlers $handlers)
