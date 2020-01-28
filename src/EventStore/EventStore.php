3ff95375f588066e8dcde0cbf587d0064497dcb6
Maik Hilker <maik@hilker.io>
2020-01-28 16:36:09 +0100

    docblock

--- src/EventStore/EventStore.php
+++ src/EventStore/EventStore.php
@@ -5,10 +5,17 @@ declare(strict_types=1);
 namespace Commander\EventStore;
 
 use Commander\Aggregate\AggregateId;
+use Commander\EventStore\Exception\EventStoreException;
 
 interface EventStore
 {
+    /**
+     * @throws EventStoreException
+     */
     public function store(StorableEvents $events): void;
 
+    /**
+     * @throws EventStoreException
+     */
     public function load(AggregateId $id): StorableEvents;
 }
