34b49ff777a23857d78b5fe3af858e628c3cc230
Maik Hilker <maik@hilker.io>
2020-01-31 08:55:24 +0100

    schema update

--- schema.sql
+++ schema.sql
@@ -1,16 +1,12 @@
-CREATE TABLE `events` (
-  `event_id` int(11) NOT NULL,
-  `aggregate_id` char(36) CHARACTER SET utf8mb4 NOT NULL,
-  `occurred_on` datetime NOT NULL,
-  `event_type` varchar(256) CHARACTER SET utf8mb4 NOT NULL,
-  `payload` json DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
-ALTER TABLE `events`
-  ADD PRIMARY KEY (`event_id`),
-  ADD UNIQUE KEY `event_id_UNIQUE` (`event_id`),
-  ADD KEY `aggregate_id_event_class` (`aggregate_id`,`event_type`);
-
-ALTER TABLE `events`
-  MODIFY `event_id` int(11) NOT NULL AUTO_INCREMENT;
-COMMIT;
+CREATE TABLE `events`
+(
+    `id`           int(11) unsigned                                              NOT NULL AUTO_INCREMENT,
+    `aggregate_id` char(36) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci     NOT NULL,
+    `occurred_on`  datetime                                                      NOT NULL,
+    `topic`        varchar(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+    `payload`      json DEFAULT NULL,
+    PRIMARY KEY (`id`),
+    KEY `idx_aggregate-id_topic` (`aggregate_id`, `topic`)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4
+  COLLATE = utf8mb4_general_ci;
